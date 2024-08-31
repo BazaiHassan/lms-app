d1679a0f59ba933cc70ec7d225434624d563aa47
bazai hassan <bazaee.hassan@gmail.com>
2024-08-31 11:15:56 +0330

    improve: async

--- backend/db/db_setup.py
+++ backend/db/db_setup.py
@@ -1,22 +1,34 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://hbazai:6099990917@localhost/lms_db"
+ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://hbazai:6099990917@localhost/lms_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={},
-    future=True
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
 )
-
-Sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
 
 Base = declarative_base()
 
+
+# DB Utilities
 def get_db():
-    db = Sessionmaker()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+async def async_get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.commit()
+
